96b3f5574afb7d1fa32d3bbea400bcc0d7dad038
Sebastian Jug <sejug@redhat.com>
2018-07-11 10:07:02 -0400

    WIP

--- test/extended/cluster/metrics/metrics.go
+++ test/extended/cluster/metrics/metrics.go
@@ -7,12 +7,9 @@ import (
 )
 
 const (
-	marker_name string = "cluster_loader_marker"
+	markerName        string = "cluster_loader_marker"
+	testDurationLabel string = "TestDuration"
 )
-
-type Metrics interface {
-	printLog() error
-}
 
 type BaseMetrics struct {
 	// To let the 3rd party know that this log entry is important
@@ -20,28 +17,22 @@ type BaseMetrics struct {
 	Marker string `json:"marker"`
 	Name   string `json:"name"`
 	Type   string `json:"type"`
+	TestDuration
 }
 
 type TestDuration struct {
-	BaseMetrics
-	StartTime    time.Time     `json:"startTime"`
-	TestDuration time.Duration `json:"testDuration"`
-}
-
-func (td TestDuration) printLog() error {
-	b, err := json.Marshal(td)
-	fmt.Println(string(b))
-	return err
+	StartTime time.Time     `json:"startTime,omitempty"`
+	Duration  time.Duration `json:"testDuration,omitempty"`
 }
 
 func (td TestDuration) MarshalJSON() ([]byte, error) {
 	type Alias TestDuration
 	return json.Marshal(&struct {
 		Alias
-		TestDuration string `json:"testDuration"`
+		Duration string `json:"testDuration"`
 	}{
-		Alias:        (Alias)(td),
-		TestDuration: td.TestDuration.String(),
+		Alias:    (Alias)(td),
+		Duration: td.Duration.String(),
 	})
 }
 
@@ -49,7 +40,7 @@ func (td *TestDuration) UnmarshalJSON(b []byte) error {
 	var err error
 	type Alias TestDuration
 	s := &struct {
-		TestDuration string `json:"testDuration"`
+		Duration string `json:"testDuration"`
 		*Alias
 	}{
 		Alias: (*Alias)(td),
@@ -57,30 +48,61 @@ func (td *TestDuration) UnmarshalJSON(b []byte) error {
 	if err := json.Unmarshal(b, &s); err != nil {
 		return err
 	}
-	td.TestDuration, err = time.ParseDuration(s.TestDuration)
+	td.Duration, err = time.ParseDuration(s.Duration)
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func LogMetrics(metrics []Metrics) error {
+func (bm *BaseMetrics) UnmarshalJSON(data []byte) error {
+	//type Alias BaseMetrics
+	//tmp := &struct {
+	//	TestDuration
+	//	*Alias
+	//}{
+	//	TestDuration: TestDuration{},
+	//	Alias:        (*Alias)(bm),
+	//}
+	fmt.Printf("UnmarshalJSON: Data input: %+v\n", string(data))
+	tmp := struct {
+		Marker string `json:"marker"`
+		Name   string `json:"name"`
+		Type   string `json:"type"`
+	}{}
+	err := json.Unmarshal(data, &tmp)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("UnmarshalJSON: Post unmarshal struct: %+v\n", tmp)
+
+	bm.Marker = tmp.Marker
+	bm.Name = tmp.Name
+	bm.Type = tmp.Type
+
+	return nil
+}
+
+func LogMetrics(metrics []BaseMetrics) error {
 	for _, m := range metrics {
-		err := m.printLog()
+		b, err := json.Marshal(m)
 		if err != nil {
 			return err
 		}
+		fmt.Println(string(b))
 	}
 	return nil
 }
 
-func NewTestDuration(name string, startTime time.Time, testDuration time.Duration) TestDuration {
-	return TestDuration{
-		BaseMetrics: BaseMetrics{
-			Marker: marker_name,
-			Name:   name,
-			Type:   fmt.Sprintf("%T", (*TestDuration)(nil))[1:]},
-		StartTime:    startTime,
-		TestDuration: testDuration,
+func NewTestDuration(name string, startTime time.Time, testDuration time.Duration) BaseMetrics {
+	return BaseMetrics{
+		Marker: markerName,
+		Name:   name,
+		Type:   testDurationLabel,
+		TestDuration: TestDuration{
+			StartTime: startTime,
+			Duration:  testDuration,
+		},
 	}
 }
